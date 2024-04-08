7f158401391d3259dac7657e74b857c78764182a
Irfan Ahmad <irfanahmad2959@gmail.com>
2024-04-08 17:05:03 +0500

    Update business_profiling.py

--- business_profiling.py
+++ business_profiling.py
@@ -3,7 +3,7 @@ import pandas as pd
 import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -26,7 +26,7 @@ class WebScraper:
         query = self.query.replace(" ", "%20")
         link = f'https://www.google.com/localservices/prolist?g2lbs=AIQllVyNk2Pbqnph9O2BVURId3NUsNUzfgDLCKbpLww_PzyAgLK3hD-UysZ1_-oPaC3Rtu7bmcdKTQd3Qq5FwsvUTCe-LQAGbQt8u-mq0xyGbl3SBhdeR-w%3D&hl=en-PK&gl=pk&ssta=1&oq=salon%20in%20usa&src=2&sa=X&q={query}&ved=0&scp=Cg9nY2lkOnVuaXZlcnNpdHkqClVuaXZlcnNpdHk%3D'
 
-        options = Options()
+        options = webdriver.ChromeOptions()
         # options.add_argument("--headless")
         self.driver = webdriver.Chrome(service=Service('chromedriver'), options=options)
         self.driver.get(link)
